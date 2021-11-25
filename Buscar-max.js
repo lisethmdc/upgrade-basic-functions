e3534eea2a1b73b581b34d99928514c5f328bb08
lisethmdc <lismc1992@gmail.com>
2021-11-25 16:25:53 +0100

    Iteration#1

---
+++ Buscar-max.js
@@ -0,0 +1,9 @@
+function sum(numberOne , numberTwo) {
+    if(numberOne > numberTwo) {
+        console.log(numberOne)
+    }else{
+        console.log(numberTwo)
+    }
+  };
+
+  console.log(sum(5,1));
